0cb6c42f8e8e7ae2e8980cfe1622ce3e03a57e93
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2023-01-08 19:06:43 +0530

    insert many users

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -20,3 +20,12 @@ export const getUsers = async (req, res) => {
     }
 
 }
+export const insertManyUsers = async(req, res) => {
+    try {
+        console.log(req.body);
+        let result = await User.insertMany(req.body.data)
+        res.status(200).json(result)
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import {createUser, getUsers} from "../controller/userController.js"
+import {createUser, getUsers, insertManyUsers} from "../controller/userController.js"
 
 const router = Router();
 
 router.post("/create-user", createUser)
-router.get("/get-user",getUsers)
+router.get("/get-user", getUsers)
+router.post("/inset-many-user",insertManyUsers)
+
 
 
 
